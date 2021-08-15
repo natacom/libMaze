7923a67d84d99f8de82d61ca7faa7dbd42615d3b
natacom <4712112+natacom@users.noreply.github.com>
2021-08-16 01:28:37 +0900

    Add a draft and test

--- libMaze/libMaze.h
+++ libMaze/libMaze.h
@@ -1,13 +1,13 @@
 #pragma once
 
-#ifdef TESTLIBRARY_EXPORTS					// Automatically defined by Visual Studio.
-#define TESTLIBRARY __declspec(dllexport)	// This is used when this file is built as a dll.
+#ifdef LIBMAZE_EXPORTS					// Automatically defined by Visual Studio.
+#define LIBMAZE __declspec(dllexport)	// This is used when this file is built as a dll.
 #else
-#define TESTLIBRARY __declspec(dllimport)	// This is used when other file includes this header.
+#define LIBMAZE __declspec(dllimport)	// This is used when other file includes this header.
 #endif
 
-extern "C" TESTLIBRARY void Generate(int row, int column);
-extern "C" TESTLIBRARY bool ExistTopWall(int row, int column);
-extern "C" TESTLIBRARY bool ExistRightWall(int row, int column);
-extern "C" TESTLIBRARY bool ExistBottomWall(int row, int column);
-extern "C" TESTLIBRARY bool ExistLeftWall(int row, int column);
+extern "C" LIBMAZE void Generate(int rowNum, int columnNum);
+extern "C" LIBMAZE bool ExistTopWall(int row, int column);
+extern "C" LIBMAZE bool ExistRightWall(int row, int column);
+extern "C" LIBMAZE bool ExistBottomWall(int row, int column);
+extern "C" LIBMAZE bool ExistLeftWall(int row, int column);
